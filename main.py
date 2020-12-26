4d4bdcfb2ea641c4afc659b107cc126e528d34db
pzhuchkov <pzhuchkov@gmail.com>
2020-12-26 23:42:43 +0300

    download books from tululu.org

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import requests
 import urllib3
+from pathlib import Path
 
 
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+BOOKS_FOLDER = 'books'
+URL_PATTERN = 'https://tululu.org/txt.php?id={}'
 
-url = 'https://tululu.org/txt.php?id=32168'
 
-response = requests.get(url, verify=False)
-response.raise_for_status()
+def download_book(url, book_id, books_folder):
+    response = requests.get(url, verify=False)
+    response.raise_for_status()
+    with open(f'{books_folder}/book_{book_id}.txt', 'w') as file:
+        file.write(response.text)
 
-with open('book.txt', 'w') as file:
-    file.write(response.text)
+
+def download_books(count):
+    for book_id in range(1, count+1):
+        url = URL_PATTERN.format(book_id)
+        download_book(url, book_id, BOOKS_FOLDER)
+
+
+if __name__ == '__main__':
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    Path(BOOKS_FOLDER).mkdir(exist_ok=True)
+    download_books(10)
