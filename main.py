0c56a7fd72916dcb3e2a68b8cbceaac8013caf20
pzhuchkov <pzhuchkov@gmail.com>
2020-12-27 18:51:27 +0300

    download book covers

--- main.py
+++ main.py
@@ -11,48 +11,55 @@ from urllib.parse import urljoin
 BOOKS_FOLDER = 'books'
 BOOK_URL = 'https://tululu.org/b{}'
 DOWNLOAD_TXT_URL = 'https://tululu.org/txt.php?id={}'
+IMAGES_FOLDER = 'images'
 
 
-def download_txt(url, filename, books_folder):
+def download_txt(response, filename, books_folder=BOOKS_FOLDER):
     Path(books_folder).mkdir(exist_ok=True)
-    try:
-        response = requests.get(url, verify=False)
-        response.raise_for_status()
-        check_for_redirect(response)
-    except HTTPError:
-        pass
-    else:
-        filepath = os.path.join(
-            books_folder, f"{sanitize_filename(filename)}.txt"
-        )
-        with open(filepath, 'w') as file:
-            file.write(response.text)
-        return filepath
+    filepath = os.path.join(
+        books_folder, f"{filename}.txt"
+    )
+    with open(filepath, 'w') as file:
+        file.write(response.text)
+    return filepath
 
 
-def download_image(url, filename, books_folder):
-    pass
+def download_image(url, images_folder=IMAGES_FOLDER):
+    Path(images_folder).mkdir(exist_ok=True)
+    response = requests.get(url, verify=False)
+    response.raise_for_status()
+    filename = url.split('/')[-1]
+    filepath = os.path.join(images_folder, filename)
+    with open(filepath, 'wb') as file:
+        file.write(response.content)
 
 
-def download_books(url, start, end, books_folder=BOOKS_FOLDER):
+def download_books(url, start, end):
     for book_id in range(start, end + 1):
         book_txt_url = url.format(book_id)
-        filename = get_filename(book_id) or str(book_id)
-        download_txt(book_txt_url, filename, books_folder)
+        try:
+            response = requests.get(book_txt_url, verify=False)
+            response.raise_for_status()
+            check_for_redirect(response)
+        except HTTPError:
+            pass
+        else:
+            filename, image_url = get_bookinfo(book_id)
+            print(filename, image_url)
+            # download_txt(response, filename)
+            download_image(image_url)
 
 
-def get_filename(book_id, url=BOOK_URL):
+def get_bookinfo(book_id, url=BOOK_URL):
     book_url = url.format(book_id)
-    try:
-        response = requests.get(book_url, verify=False)
-        response.raise_for_status()
-    except HTTPError:
-        pass
-    else:
-        soup = BeautifulSoup(response.text, 'lxml')
-        book_name = soup.find('h1').text.split('::')[0].strip()
-        filename = f"{book_id}. {book_name}"
-        return filename
+    response = requests.get(book_url, verify=False)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    book_name = soup.find('h1').text.split('::')[0].strip()
+    filename = f"{book_id}. {sanitize_filename(book_name)}"
+    image_url = soup.find(class_='bookimage').find('img')['src']
+    image_url_full = urljoin(url, image_url)
+    return filename, image_url_full
 
 
 def check_for_redirect(response):
