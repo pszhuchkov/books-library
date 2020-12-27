e39268990e26b6985c25626ab96556ee8989a337
pzhuchkov <pzhuchkov@gmail.com>
2020-12-27 10:41:35 +0300

    Add check_for_redirect function

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
 import requests
 import urllib3
 from pathlib import Path
+from requests.exceptions import HTTPError
 
 
 BOOKS_FOLDER = 'books'
-URL_PATTERN = 'https://tululu.org/txt.php?id={}'
+URL = 'https://tululu.org/txt.php'
 
 
-def download_book(url, book_id, books_folder):
-    response = requests.get(url, verify=False)
-    response.raise_for_status()
-    with open(f'{books_folder}/book_{book_id}.txt', 'w') as file:
-        file.write(response.text)
+def download_book(url, books_folder, params):
+    try:
+        response = requests.get(url, params=params, verify=False)
+        response.raise_for_status()
+        check_for_redirect(response)
+    except HTTPError:
+        print(f"{params['id']}: redirection")
+    else:
+        with open(f"{books_folder}/book_{params['id']}.txt", 'w') as file:
+            file.write(response.text)
 
 
-def download_books(count):
-    for book_id in range(1, count+1):
-        url = URL_PATTERN.format(book_id)
-        download_book(url, book_id, BOOKS_FOLDER)
+def download_books(url, books_folder, count):
+    for book_id in range(1, count + 1):
+        params = {'id': book_id}
+        download_book(url, books_folder, params)
+
+
+def check_for_redirect(response):
+    if response.history:
+        raise HTTPError
 
 
 if __name__ == '__main__':
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
     Path(BOOKS_FOLDER).mkdir(exist_ok=True)
-    download_books(10)
+    download_books(URL, BOOKS_FOLDER, 10)
