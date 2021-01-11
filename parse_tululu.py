73963c9f9f22cd2db3d63142f69c58c88a04f4e1
pzhuchkov <pzhuchkov@gmail.com>
2021-01-11 18:07:41 +0300

    Refactor function

--- parse_tululu.py
+++ parse_tululu.py
@@ -76,8 +76,8 @@ def get_book_properties(book_id, url=BOOK_URL):
 
 def parse_book_page(html, url):
     soup = BeautifulSoup(html, 'lxml')
-    book_name = soup.find('h1').text.split('::')[0].strip()
-    author = soup.find('h1').text.split('::')[1].strip()
+    book_name, author, *_ = soup.find('h1').text.split('::')
+    book_name, author = book_name.strip(), author.strip()
     relative_image_url = soup.find(class_='bookimage').find('img')['src']
     absolute_image_url = urljoin(url, relative_image_url)
     raw_comments = soup.find_all(class_='texts')
