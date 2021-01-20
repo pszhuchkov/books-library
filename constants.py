46244a647105ba92b012b4e0452d8f076e96f9a7
pzhuchkov <pzhuchkov@gmail.com>
2021-01-20 20:48:11 +0300

    Add comment

--- constants.py
+++ constants.py
@@ -2,4 +2,4 @@ BOOK_URL = 'https://tululu.org/b{}/'
 DOWNLOAD_TXT_URL = 'https://tululu.org/txt.php?id={}'
 BOOKS_DIRNAME = 'books'
 IMAGES_DIRNAME = 'images'
-COLLECTION_URL = 'https://tululu.org/l55/'
+COLLECTION_URL = 'https://tululu.org/l55/'  # Fantastic
