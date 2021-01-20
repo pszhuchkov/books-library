c75c4b5f21668ff00a86e6da964ebfa483db10de
pzhuchkov <pzhuchkov@gmail.com>
2021-01-20 19:09:57 +0300

    Add constants

---
+++ constants.py
@@ -0,0 +1,5 @@
+BOOK_URL = 'https://tululu.org/b{}/'
+DOWNLOAD_TXT_URL = 'https://tululu.org/txt.php?id={}'
+BOOKS_DIRNAME = 'books'
+IMAGES_DIRNAME = 'images'
+COLLECTION_URL = 'https://tululu.org/l55/'
